527280f868f6034f3ce93b0f722fa995573963ef
Rutik Patel <rutikpatel3660@gmail.com>
2021-12-02 16:46:36 -0500

    code  access user location

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import geoLocation from './geoLocation';
+import axios from 'axios'
+
 
 export default function App() {
+  
+  const [data, setData] = useState();
+  const location = geoLocation();
+
+  const apiData = async () =>{
+    const lat= location.latitude
+    const lng = location.longitude
+    //const api = '6a5d69a68de78bb14d0a82afd064f3e0'
+    const api = 'a6e125fb1106bdf660a722513e644e3b'
+    //const api = 'dabd43091243ee5e79e0ebb05c32db35'
+    //const api = '3e0927d5a13dcc4deaa2653db2c19ad5'
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=hourly,daily,minutely&appid=${api}`
+    const req = axios.post(url);
+    const res = await req;
+    setData(
+      res.data
+    )
+  }
+  useEffect(() => {
+    apiData()  
+  },[location])
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <View>
+    <Text>
+      {JSON.stringify(data)}
+    </Text>
     </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
